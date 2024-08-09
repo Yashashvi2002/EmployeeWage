33c74a6f44d863ab2b1d01700e92feb737bce2e8
Yashashvi2002 <yashashvidevadiga@gmail.com>
2024-08-09 13:14:23 +0530

    Add PartTime Employee and Wage

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 
-namespace EmployeeWage
+namespace Employee
 {
-    internal class Program
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Constants
+            int IS_PART_TIME = 1;
+            int INT_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+
+            //Variables
+            int empHours = 0;
+            int empWage = 0;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 3);
+
+            if (empCheck == INT_FULL_TIME)
+            {
+                empHours = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHours = 4;
+
+            }
+            else
+            {
+                empHours = 0;
+            }
+            empWage = empHours * EMP_RATE_PER_HOUR;
+            Console.WriteLine(empWage);
         }
     }
 }
