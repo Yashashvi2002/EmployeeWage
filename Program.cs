f2d5e883fa6590a5b2306b8aebb13489822db372
Yashashvi2002 <yashashvidevadiga@gmail.com>
2024-08-09 13:10:34 +0530

    Check Employee Present or Absent

--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace EmployeeWage
+namespace Employee
 {
-    internal class Program
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Constants
+            int INT_FULL_TIME = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == INT_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
