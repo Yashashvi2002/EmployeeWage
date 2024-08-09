cd496e17f328a152ba80e195a57f1b689ce5e330
Yashashvi2002 <yashashvidevadiga@gmail.com>
2024-08-09 13:12:17 +0530

    Calculate Employee Daily Wage

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
-namespace EmployeeWage
+namespace Employee
 {
-    internal class Program
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Constants
+            int INT_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+
+            //Variables
+            int empHours = 0;
+            int empWage = 0;
+            Random random = new Random();
+
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == INT_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+                empHours = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHours = 0;
+            }
+            empWage = empHours * EMP_RATE_PER_HOUR;
+            Console.WriteLine(empWage);
         }
     }
 }
