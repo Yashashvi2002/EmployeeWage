2bbbb28577b223e7e356698c1fead11dc9e0366a
Yashashvi2002 <yashashvidevadiga@gmail.com>
2024-08-28 16:45:56 +0530

    Employee Wage

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace EmployeeWage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Employee Wage");
         }
     }
 }
