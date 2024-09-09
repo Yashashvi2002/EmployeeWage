1704580179006b5f0a81cb2e112f2a6800eeaac8
Yashashvi2002 <yashashvidevadiga@gmail.com>
2024-09-09 14:41:56 +0530

    Store the Daily Wage along with Total Wage

--- Program.cs
+++ Program.cs
@@ -1,12 +1,109 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace EmployeeWage
 {
-    internal class Program
+    public class EmpWageBuilder : IComputeEmpWage
+    {
+        // Constants
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+
+        private LinkedList<CompanyEmpWage> companyEmpWageList;
+        private Dictionary<string, CompanyEmpWage> companyToEmpWageMap;
+        public EmpWageBuilder()
+        {
+            this.companyEmpWageList = new LinkedList<CompanyEmpWage>();
+            this.companyToEmpWageMap = new Dictionary<string, CompanyEmpWage>();
+        }
+
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
+            this.companyEmpWageList.AddLast(companyEmpWage);
+            this.companyToEmpWageMap.Add(company, companyEmpWage);
+        }
+        public void computeEmpWage()
+        {
+            foreach (CompanyEmpWage companyEmpWage in this.companyEmpWageList)
+            {
+                companyEmpWage.setTotalEmpWage(this.computeEmpWage(companyEmpWage));
+                Console.WriteLine(companyEmpWage.toString());
+            }
+
+        }
+
+        private int computeEmpWage(CompanyEmpWage companyEmpWage)
+        {
+            // Variables
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            // Computation
+            while (totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                int dailyWage = empHrs * companyEmpWage.empRatePerHour;
+                Console.WriteLine("Day#: " + totalWorkingDays + " Emp Hr: " + empHrs + " Daily wage: " + dailyWage);
+            }
+            return totalEmpHrs * companyEmpWage.empRatePerHour;
+        }
+    }
+
+    interface IComputeEmpWage
+    {
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+        public void computeEmpWage();
+
+    }
+
+    public class CompanyEmpWage
+    {
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWage;
+
+        public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for Company: " + this.company + " is: " + this.totalEmpWage;
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Employee Wage");
+            EmpWageBuilder empWageBuilder = new EmpWageBuilder();
+            empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
         }
     }
 }
